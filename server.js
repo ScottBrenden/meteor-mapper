6f593fd783fd1e24fb098dd60c0e35c04d531b79
Kurt Maurer <kurtrm@gmail.com>
2017-04-03 12:37:16 -0700

    Started to add a proxy, but it's not required.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 // const conString = 'postgres://postgres:1234@localhost:5432/';
 const client = new pg.Client();
 client.connect();
@@ -12,6 +14,13 @@ client.on('error', function(error) {
   console.error(error);
 });
 app.use(express.static('./public'));
+
+// function proxyGitHUb(request, response) {
+//   console.log('Routing GitHub request for', request.params[0]);
+//   (request({
+//     url: `https://`
+//   }))
+// }
 
 
 app.get('/', (request, response) => response.sendFile('index.html', {root: '.'}));
