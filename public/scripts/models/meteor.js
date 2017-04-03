f37cc091e2d75ecb344e61a0163dad800f0ec2cb
Scott Brenden <scott.b.brenden@gmail.com>
2017-04-03 16:26:33 -0700

    mostly working... pre ajax call in server.js

--- public/scripts/models/meteor.js
+++ public/scripts/models/meteor.js
@@ -10,7 +10,7 @@ Meteor.loadAll = data => {
   data.map(ele => Meteor.all.push(new Meteor(ele)));
 };
 
-Meteor.fetchAll = (callback) => {
+Meteor.fetchAll = () => {
 return $.ajax({
   url: "https://data.nasa.gov/resource/y77d-th95.json",
   type: "GET",
@@ -19,11 +19,7 @@ return $.ajax({
     "$$app_token" : ""
   }
 }).done(function(data) {
-//console.log("Retrieved " + data.length + " records from the dataset!");
-  //console.log(data);
- //(data.map((e) => e.name);
  Meteor.loadAll(data);
- callback();
 });
 };
 //
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// const conString = 'postgres://postgres:1234@localhost:5432/';
-const client = new pg.Client();
+const conString = 'postgres://postgres:flight19@localhost:5432/meteors';
+const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
   console.error(error);
@@ -15,4 +15,43 @@ client.on('error', function(error) {
 app.use(express.static('./public'));
 
 app.get('/', (request, response) => response.sendFile('index.html', {root: '.'}));
+
+loadDB();
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
+
+
+function loadMeteors(){
+  console.log('in loadMeteors');
+  client.query('SELECT COUNT(*) FROM meteors')
+  .then(result => {
+    if(!parseInt(result.rows[0].count)) {
+      console.log('in loadMeteors if');
+      Meteor.fetchAll();
+      Meteor.all.map(ele => {
+        client.query(`
+          INSERT INTO
+          meteors(name, "year", mass, recclass, reclat, reclong)
+          VALUES $1, $2, $3, $4, $5, $6
+          `,
+          [ele.name, ele.year, ele.mass, ele.recclass, ele.reclat, ele.reclong]
+        ).catch(console.error);
+      })
+    }
+  })
+}
+
+function loadDB(){
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    meteors (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        "year" DATE,
+        mass DECIMAL,
+        recclass TEXT,
+        reclat DECIMAL,
+        reclong DECIMAL
+    )`
+  ).then(loadMeteors).catch(console.error);
+}
