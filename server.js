8ff538df04c73330bacb720353482c25523c5f31
Scott Brenden <scott.b.brenden@gmail.com>
2017-04-03 14:28:45 -0700

    no you

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const conString = 'postgres://postgres:1234@localhost:5432/';
+const conString = 'postgres://postgres:flight19@localhost:5432/';
 const client = new pg.Client();
 client.connect();
 client.on('error', function(error) {
