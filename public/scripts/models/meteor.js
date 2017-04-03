9d4b3924689f2dd5a25c1e8c83be37291c348a94
Erik Enderlein <erik.end@gmail.com>
2017-04-03 13:58:20 -0500

    made meteor.js take data from the API and convert it into an array of objects.

--- public/scripts/models/meteor.js
+++ public/scripts/models/meteor.js
@@ -1,3 +1,29 @@
 'use strict';
 
-alert('hello world!');
+function Meteor(opts) {
+  Object.keys(opts).forEach(e => this[e] = opts[e]);
+}
+Meteor.all = [];
+
+Meteor.loadAll = rows => {
+  rows.map(ele => Meteor.all.push(new Meteor(ele)));
+};
+
+Meteor.fetchAll = () => {
+return $.ajax({
+  url: "https://data.nasa.gov/resource/y77d-th95.json",
+  type: "GET",
+  data: {
+    "$limit" : 5000,
+    "$$app_token" : ""
+  }
+}).done(function(data) {
+//console.log("Retrieved " + data.length + " records from the dataset!");
+  //console.log(data);
+ //(data.map((e) => e.name);
+ Meteor.loadAll(data);
+});
+};
+
+Meteor.fetchAll();
+console.log(Meteor.all);
