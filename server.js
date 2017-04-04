6d807be4935398fcf418f0a0c14e589535768d07
Kurt Maurer <kurtrm@gmail.com>
2017-04-04 13:12:52 -0700

    Added a process.env.DATABASE_URL.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const nasaURL = 'https://data.nasa.gov/resource/y77d-th95.json';
 //const conString = 'postgres://postgres:potatobabe@localhost:5432/meteors';
 //const conString = 'postgres://postgres:1234@localhost:5432/meteors';
 //const conString = 'postgres://postgres:flight19@localhost:5432/meteors';
-const conString = 'postgres://postgres:hofbrau@localhost:5432/meteors';
+const conString = process.env.DATABASE_URL || 'postgres://postgres:hofbrau@localhost:5432/meteors';
 const client = new pg.Client(conString);
 let nasaData = [];
 
