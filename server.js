f8b63c1413eb715d9215cd86a359c50ee17d4d1b
Scott Brenden <scott.b.brenden@gmail.com>
2017-04-04 11:19:12 -0700

    constring called and parsetInt to parseFloat

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
- const conString = 'postgres://postgres:flight19@localhost:5432/meteors';
+const conString = 'postgres://postgres:flight19@localhost:5432/meteors';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(error) {
