89fd8061105304f39e513f1fc6ea78c099d23b83
Scott Brenden <scott.b.brenden@gmail.com>
2017-04-04 14:27:28 -0700

    adding feature for each meteor

--- public/scripts/models/map.js
+++ public/scripts/models/map.js
@@ -1,86 +1,86 @@
-// (function(module){
-  'use strict';
+'use strict';
 
 //This pulls the google maps API.
 (function(module){
-  function initMap() {
-    // Meteor.fetchAll();
+function initMap() {
+  // Meteor.fetchAll();
 
-    var stylesArray = [
-      {
-        featureType: "all",
-        stylers: [
-          { hue: "#00ffe6" },
-          { saturation: -20 }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [
-          { lightness: 100 },
-          { visibility: "simplified" }
-        ]
-      },
-      {
-        featureType: "road",
-        elementType: "labels",
-        stylers: [
-          { visibility: "off" }
-        ]
-      }
-    ];
+  var stylesArray = [
+    {
+      featureType: "all",
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
 
-    let mapOptions = {
-      zoom: 15,
-      styles: stylesArray,
-      center: new google.maps.LatLng(47.618217, -122.351832),
-      mapTypeId: google.maps.MapTypeId.STREET,
-      zoomControl: true,
-      zoomControlOptions: {
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      }
-    };
+  let mapOptions = {
+    zoom: 15,
+    styles: stylesArray,
+    center: new google.maps.LatLng(47.618217, -122.351832),
+    mapTypeId: google.maps.MapTypeId.STREET,
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    }
+  };
 
-    var uluru = {lat: 47.618217, lng: -122.351832};
+  var uluru = {lat: 47.618217, lng: -122.351832};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-
-    //add custom icons
-    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-    let icons = {
-      meteors: {
-          icon: iconBase + 'images/test-meteor-icon.png'
-      }
-    };
-
-    google.maps.event.addDomListener(window, 'resize', function() {
-      var center = map.getCenter();
-      google.maps.event.trigger(map, 'resize');
-      map.setCenter(center);
-    });
-    // var marker = new google.maps.Marker({
-    //   position: uluru,
-    //   map: map
-    // });
-    let locations = Meteor.all.map(e => {
-      e.reclat = parseFloat(e.reclat);
-      e.reclong = parseFloat(e.reclong);
-      let newPosition = {lat: e.reclat, lng: e.reclong};
-      new google.maps.Marker({
-      position: newPosition,
-      map: map,
-      type: 'meteors'
-    });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
   });
-  }
-  module.initMap = initMap;
-})(window);
-// function renderMarkers(){
-// }
-  // module.map = map;
 
-// })(window);
+  google.maps.event.addDomListener(window, 'resize', function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+  });
+
+  let features = Meteor.all.map(meteor => {
+    mLat = parseFloat(meteor.reclat);
+    mLong = parseFloat(meteor.reclong);
+
+    position: new google.maps.LatLng(mLat, mLong),
+    type: 'meteor';
+  });
+  console.log(features);
+
+
+  let locations = Meteor.all.map(e => {
+    e.reclat = parseFloat(e.reclat);
+    e.reclong = parseFloat(e.reclong);
+    let newPosition = {lat: e.reclat, lng: e.reclong};
+    let marker = new google.maps.Marker({
+    position: newPosition,
+    map: map
+  });
+    let infowindow = new google.maps.InfoWindow({
+      content: e.name
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    })
+});
+
+
+}
+module.initMap = initMap;
+})(window);
