263924b1606437bd490118aa484dfe8a762be4f1
Erik Enderlein <erik.end@gmail.com>
2017-04-05 16:49:15 -0500

    added mass filter

--- public/scripts/views/meteorView.js
+++ public/scripts/views/meteorView.js
@@ -4,16 +4,46 @@ const meteorView = {};
 
 meteorView.handleDecadeFilter = callback => {
 $('#filter-one').on('change', function() {
-  $.ajax({url: '/meteors/find', method:'GET', headers: {val: this.value + '-01-01', val2: parseInt(this.value) + 10 + '-01-01'}})
-
-  // '/meteors/find', {val: this.value})
-  .then(results => Meteor.loadAll(results));
-  callback();
-
-    // {console.log(results.map(e => parseInt(JSON.stringify(e.year).slice(1, 5)).filter(i => i >= this.value).filter(j => j < this.value + 10)  )  )});
-
-  //this.value;
+  if (this.value){
+  $('#filter-two').val('');
+  $.ajax({url: '/meteors/decade', method:'GET', headers: {val: this.value + '-01-01', val2: parseInt(this.value) + 10 + '-01-01'}})
+  .then(results => {
+    Meteor.loadAll(results)
+    callback();
+  });
+} else {
+  Meteor.fetchAll(initAutocomplete);
+}
 });
 
 };
+meteorView.handleMassFilter = callback =>{
+  $('#filter-two').on('change', function(){
+    if (this.value){
+      switch(this.value){
+        case 'small':
+          var valueOne = 0;
+          var valueTwo = 1000;
+          break;
+        case 'medium':
+          var valueOne = 1000;
+          var valueTwo = 10000;
+          break;
+        case 'big':
+          var valueOne = 10000;
+          var valueTwo = 10000000000;
+          break;
+      }
+      $('#filter-one').val('');
+      $.ajax({url: '/meteors/mass', method: 'GET', headers: {val: valueOne, val2: valueTwo}})
+      .then(results => {
+        Meteor.loadAll(results);
+        callback();
+      });
+    } else {
+      Meteor.fetchAll(initAutocomplete);
+    }
+  });
+};
 meteorView.handleDecadeFilter(initAutocomplete);
+meteorView.handleMassFilter(initAutocomplete);
