8d1ab0514c6318091677472358e4235ce46b8241
Erik Enderlein <erik.end@gmail.com>
2017-04-05 18:38:08 -0500

    token!

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,9 +1,5 @@
 'use strict';
 
-
-// page('/', meteorView.fetchAll(initMarkers));
-
 page('/', mapController.init);
 page('/about', aboutController.init);
-
 page();
--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 const request = require('superagent');
-const nasaURL = 'https://data.nasa.gov/resource/y77d-th95.json';
-
+const nasaURL = 'https://data.nasa.gov/resource/y77d-th95.json?$$app_token=  place token here  &$limit=10000';
 // const conString = 'postgres://postgres:potatobabe@localhost:5432/meteors';
 const conString = 'postgres://postgres:1234@localhost:5432/meteors';
 
 // const conString = 'postgres://postgres:flight19@localhost:5432/meteors';
 //const conString = 'postgres://postgres:1234@localhost:5432/meteors';
 // const conString = process.env.DATABASE_URL || 'postgres://postgres:hofbrau@localhost:5432/meteors';
-
+//
 const client = new pg.Client(conString);
 let nasaData = [];
 
