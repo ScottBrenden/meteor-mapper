8472795a2dc487355e52b2e01397dd04c59a4245
Kayla Asay <kdasay16@gmail.com>
2017-04-06 16:24:10 -0700

    pic

--- public/scripts/views/aboutUsData.js
+++ public/scripts/views/aboutUsData.js
@@ -4,7 +4,7 @@ const aboutUsData = [
   {
     name:'Kayla',
     about:'My name is Kayla. I like cats and other cute critters. I was in the Army, the Army is the best branch. I have an AAS in Patisserie and Baking. I have been working in a kitchen for the past several years, so coding has been a fun new challenge for me. I look forward to see where web development takes me.',
-    img: ''
+    img: 'images/kayla.jpg'
   },
   {
     name:'Scott',
